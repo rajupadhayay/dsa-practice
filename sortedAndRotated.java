115f32c2872ee9fb82d253c000ba6b9aac0d9153
Raj Upadhayay <rajupadhayay2005@gmail.com>
2026-02-02 22:54:54 +0530

    Fix comparisonn logic in leetcode 1752 solution

--- sortedAndRotated.java
+++ sortedAndRotated.java
@@ -3,7 +3,7 @@ class sortedAndRotated {
         int count = 0;
         for(int i=0;i<nums.length;i++){
             int next = nums[(i+1)%nums.length];
-            if (nums[i]>=next) {
+            if (nums[i]>next) {
                 count++;
             }
 
