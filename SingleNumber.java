888c5823f0db7ddb35cd6edf54a8c2cff7a8709a
Raj Upadhayay <rajupadhayay2005@gmail.com>
2026-02-20 09:02:56 +0530

    Add Another optimized approach for LeetCode 136 Single Number

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class SingleNumber {
     // 1. Bruteforce Approach
     public static int singleNumber1(int[] nums) {
@@ -20,20 +21,39 @@ public class SingleNumber {
         }
         return -1;
     }
-
+// 2. Optimized Approach
     public static int singleNumber2(int nums[]){
+
         int XOR = 0;
         for(int i=0;i<nums.length;i++){
             XOR^=nums[i];
         }
-      //  XOR^=nums.length;
       return XOR;
 
     }
+//3. Optimized Approach using HashMap
+
+ public static int singleNumber3(int nums[]){
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+       
+      for(int key : map.keySet()){
+          if (map.get(key) == 1) {
+        return key;
+    }
+}
+    return -1;
+}
+    
 
     public static void main(String[] args) {
         int nums[] = { 4,1,2,1,2};
-      //  System.out.println(singleNumber1(nums));
+        System.out.println(singleNumber1(nums));
         System.out.println(singleNumber2(nums));
+        System.out.println(singleNumber3(nums));
+       
     }
 }
