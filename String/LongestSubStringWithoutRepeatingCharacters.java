53b59904a348ff22621523b4193accaf695c6fa5
Raj Upadhayay <rajupadhayay2005@gmail.com>
2026-03-04 22:14:24 +0530

    Solved LeetCode 3 Longest SubString Without Repeating Characters using Hashset with Sliding Window

---
+++ String/LongestSubStringWithoutRepeatingCharacters.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public  class LongestSubStringWithoutRepeatingCharacters {
+
+     public static int lengthOfLongestSubstring(String s) {
+     HashSet<Character> set = new HashSet<>();
+     int left =0;
+     int maxLength =0;
+     for(int i = 0;i <s.length();i++){
+        while (set.contains(s.charAt(i))) {
+          set.remove(s.charAt(left));
+          left++;
+        }
+        set.add(s.charAt(i));
+        maxLength = Math.max(maxLength, i-left+1);
+     }   
+     return maxLength;
+    }
+    public static void main(String[] args) {
+      String  s = "pwwkew";
+      System.out.println(lengthOfLongestSubstring(s));
+    }
+}
