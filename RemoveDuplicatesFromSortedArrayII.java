b117b8663b211eda9f1daaace8ed2f5867aa23ea
Raj Upadhayay <rajupadhayay2005@gmail.com>
2026-03-20 17:20:23 +0530

    Solved LeetCode 80 Remove Duplicates From Sorted Array II  using Two Pointer

---
+++ RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,19 @@
+public class RemoveDuplicatesFromSortedArrayII {
+     public static int removeDuplicates(int[] nums) {
+      int k =2;
+      int n = nums.length;
+      for(int j=2;j<nums.length;j++){
+        if(nums[j]!=nums[k-2]){
+            nums[k]=nums[j];
+         k++;
+      
+      } 
+     
+    } 
+    return k; 
+    }
+    public static void main(String[] args) {
+    int[] nums = {2,6,6,8,8};
+        System.out.println(removeDuplicates(nums));
+    }
+}
