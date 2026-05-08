1a9ee6218c478d9103bc9e7de3712d10a5d06349
Raj Upadhayay <rajupadhayay2005@gmail.com>
2026-05-08 23:08:43 +0530

    Solved LeetCode 881 Boats To Save People

---
+++ BoatsToSavePeople.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+     public static int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+      int start = 0;
+      int end = people.length-1;
+      int boats =0;
+      while(start<=end){
+        if(people[start]+people[end]<=limit)start++;
+        end--;
+        boats++;
+      } 
+      return boats; 
+    }
+
+    public static void main(String[] args) {
+       int []  people = {1,2}; 
+       int limit = 3;
+       System.out.println(numRescueBoats(people, limit));
+
+    }
+}
