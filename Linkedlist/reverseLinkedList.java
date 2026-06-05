bae043ee091391b5b8490c56898cd0bcae9e1c03
Raj Upadhayay <rajupadhayay2005@gmail.com>
2026-06-05 18:04:10 +0530

    Add Linkedlist Folder

---
+++ Linkedlist/reverseLinkedList.java
@@ -0,0 +1,54 @@
+public class reverseLinkedList{
+     public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+     private Node Head;
+    private Node Tail;
+      private int size;
+
+
+public void reverse(){
+        if (Head==null||Head.next == null) {
+            return;
+        }
+        Node prev = Head;
+        Node curr = Head.next;
+        while (curr!=null) {
+            Node next = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+            
+          
+        }
+          Head.next = null;
+            Head = prev;
+    }
+      public void print(){
+        Node temp = Head;
+        while (temp!=null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+      public static void main(String[]args){
+       reverseLinkedList   n = new reverseLinkedList();
+          n.Head = new Node(1);
+    n.Head.next = new Node(2);
+    n.Head.next.next = new Node(3);
+    n.Head.next.next.next = new Node(4);
+    n.print();
+        n.reverse();
+     n.print();
+      }
+
+
+}
