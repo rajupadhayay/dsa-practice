dde712f21b1e4e9033f5cd9e142586787e52112e
Raj Upadhayay <rajupadhayay2005@gmail.com>
2026-06-17 23:53:27 +0530

    Solved LeetCode 238 Product of Array Except Self

---
+++ ProductofArrayExceptSelf.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+
+public class ProductofArrayExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+
+        int ans[] = new int[nums.length];
+        ans[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+        int suffix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+}
+}
